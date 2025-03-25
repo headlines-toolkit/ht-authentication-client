4c6d3d2b7ab1932b1ffad359744c6d1aeba0b797
fulleni <fulleni@hotmail.com>
2025-03-25 10:05:06 +0100

    test: add tests for authentication status
    - Added tests for value inequality
    - Added tests for fromJson method
    - Added tests for toJson method

--- test/src/models/user_test.dart
+++ test/src/models/user_test.dart
@@ -10,6 +10,7 @@ void main() {
         email: 'test@example.com',
         displayName: 'Test User',
         photoUrl: 'https://example.com/photo.jpg',
+        authenticationStatus: AuthenticationStatus.unauthenticated,
         isEmailVerified: true,
       );
       final user2 = User(
@@ -17,9 +18,30 @@ void main() {
         email: 'test@example.com',
         displayName: 'Test User',
         photoUrl: 'https://example.com/photo.jpg',
+        authenticationStatus: AuthenticationStatus.unauthenticated,
         isEmailVerified: true,
       );
       expect(user1, user2);
+    });
+
+    test('supports value inequality for different authenticationStatus', () {
+      final user1 = User(
+        uid: '1',
+        email: 'test@example.com',
+        displayName: 'Test User',
+        photoUrl: 'https://example.com/photo.jpg',
+        authenticationStatus: AuthenticationStatus.unauthenticated,
+        isEmailVerified: true,
+      );
+      final user2 = User(
+        uid: '1',
+        email: 'test@example.com',
+        displayName: 'Test User',
+        photoUrl: 'https://example.com/photo.jpg',
+        authenticationStatus: AuthenticationStatus.authenticated,
+        isEmailVerified: true,
+      );
+      expect(user1, isNot(user2));
     });
 
     test('generates a UUID if uid is not provided', () {
@@ -27,13 +49,13 @@ void main() {
       expect(Uuid.isValidUUID(fromString: user.uid), isTrue);
     });
 
-    test('fromJson creates a User from a Map', () {
+    test('fromJson creates a User from a Map with unauthenticated status', () {
       final json = {
         'uid': '1',
         'email': 'test@example.com',
         'displayName': 'Test User',
         'photoUrl': 'https://example.com/photo.jpg',
-        'isAnonymous': false,
+        'authenticationStatus': 'unauthenticated',
         'isEmailVerified': true,
         'isNewUser': false,
       };
@@ -42,17 +64,58 @@ void main() {
       expect(user.email, 'test@example.com');
       expect(user.displayName, 'Test User');
       expect(user.photoUrl, 'https://example.com/photo.jpg');
-      expect(user.isAnonymous, false);
+      expect(user.authenticationStatus, AuthenticationStatus.unauthenticated);
       expect(user.isEmailVerified, true);
       expect(user.isNewUser, false);
     });
 
-    test('toJson creates a Map from a User', () {
+    test('fromJson creates a User from a Map with anonymous status', () {
+      final json = {
+        'uid': '1',
+        'email': 'test@example.com',
+        'displayName': 'Test User',
+        'photoUrl': 'https://example.com/photo.jpg',
+        'authenticationStatus': 'anonymous',
+        'isEmailVerified': true,
+        'isNewUser': false,
+      };
+      final user = User.fromJson(json);
+      expect(user.uid, '1');
+      expect(user.email, 'test@example.com');
+      expect(user.displayName, 'Test User');
+      expect(user.photoUrl, 'https://example.com/photo.jpg');
+      expect(user.authenticationStatus, AuthenticationStatus.anonymous);
+      expect(user.isEmailVerified, true);
+      expect(user.isNewUser, false);
+    });
+
+    test('fromJson creates a User from a Map with authenticated status', () {
+      final json = {
+        'uid': '1',
+        'email': 'test@example.com',
+        'displayName': 'Test User',
+        'photoUrl': 'https://example.com/photo.jpg',
+        'authenticationStatus': 'authenticated',
+        'isEmailVerified': true,
+        'isNewUser': false,
+      };
+      final user = User.fromJson(json);
+      expect(user.uid, '1');
+      expect(user.email, 'test@example.com');
+      expect(user.displayName, 'Test User');
+      expect(user.photoUrl, 'https://example.com/photo.jpg');
+      expect(user.authenticationStatus, AuthenticationStatus.authenticated);
+      expect(user.isEmailVerified, true);
+      expect(user.isNewUser, false);
+    });
+
+    test('toJson creates a Map from a User with authenticated status', () {
       final user = User(
         uid: '1',
         email: 'test@example.com',
         displayName: 'Test User',
         photoUrl: 'https://example.com/photo.jpg',
+        authenticationStatus: AuthenticationStatus.authenticated,
         isEmailVerified: true,
       );
       final json = user.toJson();
@@ -60,7 +123,26 @@ void main() {
       expect(json['email'], 'test@example.com');
       expect(json['displayName'], 'Test User');
       expect(json['photoUrl'], 'https://example.com/photo.jpg');
-      expect(json['isAnonymous'], false);
+      expect(json['authenticationStatus'], 'authenticated');
+      expect(json['isEmailVerified'], true);
+      expect(json['isNewUser'], false);
+    });
+
+    test('toJson creates a Map from a User with anonymous status', () {
+      final user = User(
+        uid: '1',
+        email: 'test@example.com',
+        displayName: 'Test User',
+        photoUrl: 'https://example.com/photo.jpg',
+        authenticationStatus: AuthenticationStatus.anonymous,
+        isEmailVerified: true,
+      );
+      final json = user.toJson();
+      expect(json['uid'], '1');
+      expect(json['email'], 'test@example.com');
+      expect(json['displayName'], 'Test User');
+      expect(json['photoUrl'], 'https://example.com/photo.jpg');
+      expect(json['authenticationStatus'], 'anonymous');
       expect(json['isEmailVerified'], true);
       expect(json['isNewUser'], false);
     });
